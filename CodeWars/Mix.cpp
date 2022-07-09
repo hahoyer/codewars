ad671f47a14c5e8b5f9fd99c2ca0bd983fdb2969
hh <hahoyer@gmail.com>
2022-07-09 04:01:16 +0200

    training for holoplot application

---
+++ CodeWars/Mix.cpp
@@ -0,0 +1,113 @@
+﻿// ReSharper disable CppPossiblyErroneousEmptyStatements
+#pragma warning( disable : 4267 ) // 'var' : conversion from 'size_t' to 'type', possible loss of data
+#include <functional>
+#include <igloo/igloo_alt.h>
+#pragma warning( default : 4267 ) // 'var' : conversion from 'size_t' to 'type', possible loss of data
+
+using namespace igloo;
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+vector<int> GetCounts(const string& s1)
+{
+  auto result = vector<int>('z' - 'a' + 1);
+  for(const auto position : s1)
+    if(position >= 'a' && position <= 'z')
+      result[position - 'a']++;
+  return result;
+}
+
+vector<size_t> GetOrderedElements(const vector<int>& first, const vector<int>& other)
+{
+  auto heads = vector<char>('z' - 'a' + 1);
+  auto maximalCounts = vector<int>('z' - 'a' + 1);
+  for(auto index = 0u; index < maximalCounts.size(); index++)
+  {
+    heads[index] = first[index] > other[index] ? '1' : first[index] < other[index] ? '2' : '=';
+    maximalCounts[index] = max(first[index], other[index]);
+  }
+
+  auto result = vector<size_t>('z' - 'a' + 1);
+  for(auto index = 0u; index < result.size(); ++index)
+    result[index] = index;
+  sort(result.begin(), result.end(), [&](size_t firstIndex, size_t otherIndex)
+  {
+    const auto countDifference = maximalCounts[firstIndex] - maximalCounts[otherIndex];
+    if(countDifference != 0)
+      return countDifference > 0;
+    if(heads[firstIndex] != heads[otherIndex])
+      return heads[firstIndex] < heads[otherIndex];
+    return firstIndex < otherIndex;
+  });
+
+  auto index = 0u;
+  for(; index < result.size() && maximalCounts[result[index]] > 1; ++index);
+  result.erase(result.begin() + index, result.end());
+  return result;
+}
+
+string GetCode(char character, int countS1, int countS2)
+{
+  string head = "=";
+  auto counts = countS1;
+  if(countS1 > countS2)
+    head = "1";
+  else if(countS1 < countS2)
+  {
+    head = "2";
+    counts = countS2;
+  }
+  auto characters = string();
+  while(counts-- > 0)
+    characters += character;
+
+  return head + ":" + characters;
+}
+
+class Mix
+{
+public:
+  static std::string mix(const std::string& s1, const std::string& s2)
+  {
+    const auto countsS1 = GetCounts(s1);
+    const auto countsS2 = GetCounts(s2);
+    const auto orderedElements = GetOrderedElements(countsS1, countsS2);
+
+    auto result = string();
+    string delimiter;
+    for(const auto element : orderedElements)
+    {
+      result += delimiter + GetCode(static_cast<char>('a' + element), countsS1[element], countsS2[element]);
+      delimiter = "/";
+    }
+
+    return result;
+  }
+};
+
+
+void testequal(std::string ans, std::string sol)
+{
+  Assert::That(ans, Equals(sol));
+}
+
+void dotest(const std::string& s1, const std::string& s2, std::string expected)
+{
+  testequal(Mix::mix(s1, s2), expected);
+}
+
+Describe(mix_Tests)
+{
+  It(Fixed_Tests)
+  {
+    std::string sol = "2:eeeee/2:yy/=:hh/=:rr";
+    dotest("Are they here", "yes, they are here", sol);
+    sol = "1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg";
+    dotest("looping is fun but dangerous", "less dangerous than coding", sol);
+  }
+};
---
+++ CodeWars/determinant.cpp
@@ -0,0 +1,93 @@
+﻿// ReSharper disable CppPossiblyErroneousEmptyStatements
+#pragma warning( disable : 4267 ) // 'var' : conversion from 'size_t' to 'type', possible loss of data
+#include <functional>
+#include <igloo/igloo_alt.h>
+#pragma warning( default : 4267 ) // 'var' : conversion from 'size_t' to 'type', possible loss of data
+
+using namespace igloo;
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<size_t>> GetPermutations(size_t count)
+{
+  if(count == 0)
+    return {{}};
+
+  vector<vector<size_t>> result;
+  const auto permutations = GetPermutations(count - 1);
+  for(const auto& permutation : permutations)
+    for(auto index = 0u; index < count; index++)
+    {
+      auto subResult = permutation;
+      subResult.insert(subResult.begin() + index, count - 1);
+      result.push_back(subResult);
+    }
+
+  return result;
+}
+
+long long GetSign(vector<size_t> const& permutation)
+{
+  long long result = 1;
+  for(auto index = 1u; index < permutation.size(); index++)
+    for(auto otherIndex = 0u; otherIndex < index; otherIndex++)
+      if(permutation[otherIndex] > permutation[index])
+        result *= -1;
+  return result;
+}
+
+long long GetProduct(const vector<vector<long long>>& value, vector<size_t> const& permutation)
+{
+  long long result = GetSign(permutation);
+  for(auto index = 0u; index < permutation.size(); index++)
+    result *= value[index][permutation[index]];
+  return result;
+}
+
+long long determinant(vector<vector<long long>> const& value)
+{
+  const auto count = value.size();
+  for(const auto& line : value)
+    Assert::That(count, Equals(line.size()));
+
+  long long result = 0;
+  int sign = 1;
+  const auto permutations = GetPermutations(count);
+  for(const auto& permutation : permutations)
+  {
+    const auto product = GetProduct(value, permutation);
+    result += product;
+    sign *= -1;
+  }
+
+  return result;
+}
+
+Describe(your_determinant_function)
+{
+  It(should_work_for_a_few_simple_square_matrices)
+  {
+    Assert::That(determinant(vector<vector<long long>>{
+                   vector<long long>{2, 5, 3},
+                   vector<long long>{1, -2, -1},
+                   vector<long long>{1, 3, 4}
+                 }), Equals(-20));
+
+    Assert::That(determinant(vector<vector<long long>>{
+                   vector<long long>{1}
+                 }), Equals(1));
+    Assert::That(determinant(vector<vector<long long>>{
+                   vector<long long>{1, 3},
+                   vector<long long>{2, 5}
+                 }), Equals(-1));
+    Assert::That(determinant(vector<vector<long long>>{
+                   vector<long long>{2, 5, 3},
+                   vector<long long>{1, -2, -1},
+                   vector<long long>{1, 3, 4}
+                 }), Equals(-20));
+  }
+};
