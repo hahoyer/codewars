49cca45d85670d0b133d9c2b563b6a76feb78d9a
hh <hahoyer@gmail.com>
2022-07-11 22:14:51 +0200

    training for holoplot application

---
+++ CodeWars/ShortestKnightMove.cpp
@@ -0,0 +1,73 @@
+﻿// ReSharper disable CppPossiblyErroneousEmptyStatements
+#pragma warning( disable : 4267 ) // 'var' : conversion from 'size_t' to 'type', possible loss of data
+#include <functional>
+#include <igloo/igloo_alt.h>
+#pragma warning( default : 4267 ) // 'var' : conversion from 'size_t' to 'type', possible loss of data
+
+using namespace igloo;
+
+namespace ShortestKnightMove
+{
+#include <string>
+  using namespace std;
+
+  constexpr size_t boardSize = 8;
+  vector<vector<int>> distanceCache;
+  const vector<pair<int, int>> moves = {{1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, 1}, {2, -1}, {-2, 1}, {-2, -1}};
+
+  bool IsValid(int x, int y)
+  {
+    return x >= 0 && x < static_cast<int>(boardSize) && y >= 0 && y < static_cast<int>(boardSize);
+  }
+
+  void InitialiseCache()
+  {
+    distanceCache = vector<vector<int>>(boardSize);
+    for(auto index = 0u; index < boardSize; index ++)
+      distanceCache[index] = vector<int>(boardSize);
+  }
+
+  int GetDistance(int x, int y, int distance, int xTarget, int yTarget)
+  {
+    if(!IsValid(x, y))
+      return boardSize * boardSize;
+
+    vector<int>::reference tested = distanceCache[x][y];
+    if(tested && tested <= distance)
+      return boardSize * boardSize;
+
+    tested = distance;
+
+    if(x == xTarget && y == yTarget)
+      return distance;
+
+    int result = boardSize * boardSize;
+    for(const auto& [dx, dy] : moves)
+    {
+      const auto stepResult = GetDistance(x + dx, y + dy, distance + 1, xTarget, yTarget);
+      if(result > stepResult)
+        result = stepResult;
+    }
+    Assert::That(result > 0);
+    return result;
+  }
+
+
+  size_t knight(std::string start, std::string finish)
+  {
+    InitialiseCache();
+    return GetDistance(start[0] - 'a', start[1] - '1', 0, finish[0] - 'a', finish[1] - '1');
+  }
+
+  Describe(Sample_Tests)
+  {
+    It(Tests)
+    {
+      Assert::That(knight("a1", "c1"), Equals(2));
+      Assert::That(knight("a1", "f1"), Equals(3));
+      Assert::That(knight("a1", "f3"), Equals(3));
+      Assert::That(knight("a1", "f4"), Equals(4));
+      Assert::That(knight("a1", "f7"), Equals(5));
+    }
+  };
+}
---
+++ CodeWars/TheBooleanOrder.cpp
@@ -0,0 +1,139 @@
+﻿// ReSharper disable CppPossiblyErroneousEmptyStatements
+#pragma warning( disable : 4267 ) // 'var' : conversion from 'size_t' to 'type', possible loss of data
+#include <igloo/igloo_alt.h>
+#pragma warning( default : 4267 ) // 'var' : conversion from 'size_t' to 'type', possible loss of data
+
+using namespace igloo;
+
+namespace TheBooleanOrder
+{
+#include <string>
+#include <vector>
+
+  using namespace std;
+
+  vector<int64_t> countsCache;
+
+  int64_t GetCombinationCounts(size_t operationCount)
+  {
+    if(operationCount < countsCache.size())
+      return countsCache[operationCount];
+    int64_t result = 0;
+    if(operationCount == 0)
+      result = 1;
+    for(size_t index = 0; index < operationCount; ++index)
+      result += GetCombinationCounts(index) * GetCombinationCounts(operationCount - index - 1);
+    countsCache.push_back(result);
+    return result;
+  }
+
+  int64_t Calculate(int64_t first, int64_t countFirst, char operation,
+                    int64_t other, int64_t countOther)
+  {
+    switch(operation)
+    {
+    case '&':
+      return first * other;
+    case '|':
+      return countFirst * other + first * countOther - first * other;
+    case '^':
+      return countFirst * other + first * countOther - 2 * first * other;
+    default:
+      return {};
+    }
+  };
+
+  int64_t Calculate(const std::string& constants, const std::string& operations, size_t start, size_t end);
+
+  int64_t CalculateStep(const std::string& constants, const std::string& operations, size_t start, size_t index,
+                        size_t end)
+  {
+    const auto operation = operations[index];
+    const auto countFirst = GetCombinationCounts(index - start);
+    const auto countOther = GetCombinationCounts(end - index - 1);
+    const auto firstResults = Calculate(constants, operations, start, index);
+    const auto otherResults = Calculate(constants, operations, index + 1, end);
+    return Calculate(firstResults, countFirst, operation, otherResults, countOther);
+  }
+
+  int64_t CalculateStepSmart(const std::string& constants, const std::string& operations, size_t start, size_t index,
+                             size_t end)
+  {
+    const auto operation = operations[index];
+    const auto countFirst = GetCombinationCounts(index - start);
+    const auto countOther = GetCombinationCounts(end - index - 1);
+
+    if(countFirst < countOther)
+    {
+      const auto firstResults = Calculate(constants, operations, start, index);
+      switch(operation)
+      {
+      case '&':
+        if(firstResults == 0)
+          return 0;
+        break;
+      case '|':
+        if(firstResults == countFirst)
+          return countFirst * countOther;
+        break;
+      default: ;
+      }
+
+      const auto otherResults = Calculate(constants, operations, index + 1, end);
+      return Calculate(firstResults, countFirst, operation, otherResults, countOther);
+    }
+    const auto otherResults = Calculate(constants, operations, index + 1, end);
+    switch(operation)
+    {
+    case '&':
+      if(otherResults == 0)
+        return 0;
+      break;
+    case '|':
+      if(otherResults == countOther)
+        return countFirst * countOther;
+      break;
+    default: ;
+    }
+
+    const auto firstResults = Calculate(constants, operations, start, index);
+    return Calculate(firstResults, countFirst, operation, otherResults, countOther);
+  }
+
+  int64_t Calculate(const std::string& constants, const std::string& operations, size_t start,
+                    size_t end)
+  {
+    if(start == end)
+      return constants[start] == 'f' ? 0 : 1;
+
+    int64_t results = 0;
+    for(auto index = start; index < end; index++)
+    {
+      const auto calculateStep = CalculateStepSmart(constants, operations, start, index, end);
+      //Assert::That(calculateStep, Equals(CalculateStep(constants, operations, start, index, end)));
+      results += calculateStep;
+    }
+    return results;
+  }
+
+  int64_t solve(const std::string& constants, const std::string& operations)
+  {
+    auto result = Calculate(constants, operations, 0, operations.length());
+    return result;
+  }
+
+  // TODO: Replace examples and use TDD development by writing your own tests
+
+  Describe(sample_tests)
+  {
+    It(basic_tests)
+    {
+      Assert::That(solve("ttftff", "|&^&&"), Equals(16));
+      Assert::That(solve("tft", "^&"), Equals(2));
+      Assert::That(solve("ttftff", "|&^&&"), Equals(16));
+      Assert::That(solve("ttftfftf", "|&^&&||"), Equals(339));
+      Assert::That(solve("ttftfftft", "|&^&&||^"), Equals(851));
+      Assert::That(solve("ttftfftftf", "|&^&&||^&"), Equals(2434));
+    }
+  };
+}
